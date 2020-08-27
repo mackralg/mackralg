19457823f0cbd2d31e34aa1469d21baba938d130
mackralg <mackralgonsalves@gmail.com>
2020-08-27 13:04:00 +0530

    commit

---
+++ precache-manifest.0da3758c7e8ffd1d1dd0955dc945e0a6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7d83efbd953dec6b61d70138dbd1ddf",
+    "url": "/mackralg/index.html"
+  },
+  {
+    "revision": "fe785de890cd5816470b",
+    "url": "/mackralg/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "e27df97641ffc7e04026",
+    "url": "/mackralg/static/css/main.1013aaa8.chunk.css"
+  },
+  {
+    "revision": "fe785de890cd5816470b",
+    "url": "/mackralg/static/js/2.cd7760c3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mackralg/static/js/2.cd7760c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e27df97641ffc7e04026",
+    "url": "/mackralg/static/js/main.79bcef99.chunk.js"
+  },
+  {
+    "revision": "dd9784c9a5c24bea70ff",
+    "url": "/mackralg/static/js/runtime-main.398bca46.js"
+  },
+  {
+    "revision": "16406c1eeaae29369be4f39bfc6c8f32",
+    "url": "/mackralg/static/media/mac.16406c1e.jpg"
+  }
+]);
