3288ec6f85a112c3dd14b454a96b87a853a7683b
mackralg <mackralgonsalves@gmail.com>
2020-08-29 00:04:15 +0530

    new build

---
+++ precache-manifest.8c2dff0e90a9257e9cdb351440673b9f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c88298e1aa6f0198dc6b235038684dc3",
+    "url": "/mackralg/index.html"
+  },
+  {
+    "revision": "de9f2c0f660b91f30520",
+    "url": "/mackralg/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "b613f38a701479f497a7",
+    "url": "/mackralg/static/css/main.1013aaa8.chunk.css"
+  },
+  {
+    "revision": "de9f2c0f660b91f30520",
+    "url": "/mackralg/static/js/2.8d94d161.chunk.js"
+  },
+  {
+    "revision": "89a6b555af4184298a5af61b72cf43aa",
+    "url": "/mackralg/static/js/2.8d94d161.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b613f38a701479f497a7",
+    "url": "/mackralg/static/js/main.3343b5a0.chunk.js"
+  },
+  {
+    "revision": "dd9784c9a5c24bea70ff",
+    "url": "/mackralg/static/js/runtime-main.398bca46.js"
+  },
+  {
+    "revision": "16406c1eeaae29369be4f39bfc6c8f32",
+    "url": "/mackralg/static/media/mac.16406c1e.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.44923fa9f8226ef1a1dce6171225d129.js"
+  "/mackralg/precache-manifest.8c2dff0e90a9257e9cdb351440673b9f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mackralg/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
