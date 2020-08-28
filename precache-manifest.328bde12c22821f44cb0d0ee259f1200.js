321be1479ed3fa797531861aa1eb303564a76733
mackralg <mackralgonsalves@gmail.com>
2020-08-29 00:55:43 +0530

    new build

--- precache-manifest.328bde12c22821f44cb0d0ee259f1200.js
+++ precache-manifest.328bde12c22821f44cb0d0ee259f1200.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1535a9b168e4c5f421988fe41ba62fc4",
+    "revision": "ccfe681a69f309b7f7fc6ec666262778",
     "url": "/mackralg/index.html"
   },
   {
-    "revision": "85eed07cbae4a7eda9a3",
+    "revision": "8a043963db72b67077dd",
     "url": "/mackralg/static/css/2.11829350.chunk.css"
   },
   {
-    "revision": "64ce2dd424fb793c221b",
+    "revision": "93417fc9f0d8a0c499e0",
     "url": "/mackralg/static/css/main.1013aaa8.chunk.css"
   },
   {
-    "revision": "85eed07cbae4a7eda9a3",
-    "url": "/mackralg/static/js/2.26de6fd3.chunk.js"
+    "revision": "8a043963db72b67077dd",
+    "url": "/mackralg/static/js/2.5ba607f9.chunk.js"
   },
   {
-    "revision": "85c1ecd325742a4c0551d2653970c6fb",
-    "url": "/mackralg/static/js/2.26de6fd3.chunk.js.LICENSE.txt"
+    "revision": "a3d5da51d8489a02931a36aa8a33f4e5",
+    "url": "/mackralg/static/js/2.5ba607f9.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "64ce2dd424fb793c221b",
-    "url": "/mackralg/static/js/main.2a9103a1.chunk.js"
+    "revision": "93417fc9f0d8a0c499e0",
+    "url": "/mackralg/static/js/main.94d067d8.chunk.js"
   },
   {
     "revision": "dd9784c9a5c24bea70ff",
