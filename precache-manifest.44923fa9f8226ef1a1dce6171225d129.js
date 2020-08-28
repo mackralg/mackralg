d82767a70e9f67cc458c117d72cbbd2742ac4ab6
mackralg <mackralgonsalves@gmail.com>
2020-08-28 23:51:26 +0530

    Initial dist subtree commit

---
+++ precache-manifest.44923fa9f8226ef1a1dce6171225d129.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3e393cb99207a2c6773975d978ecb15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de9f2c0f660b91f30520",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "a192153c631613d38740",
+    "url": "/static/css/main.1013aaa8.chunk.css"
+  },
+  {
+    "revision": "de9f2c0f660b91f30520",
+    "url": "/static/js/2.8d94d161.chunk.js"
+  },
+  {
+    "revision": "89a6b555af4184298a5af61b72cf43aa",
+    "url": "/static/js/2.8d94d161.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a192153c631613d38740",
+    "url": "/static/js/main.4f0f4913.chunk.js"
+  },
+  {
+    "revision": "5ae8de4bfe03964be277",
+    "url": "/static/js/runtime-main.8cc3ca45.js"
+  },
+  {
+    "revision": "16406c1eeaae29369be4f39bfc6c8f32",
+    "url": "/static/media/mac.16406c1e.jpg"
+  }
+]);
